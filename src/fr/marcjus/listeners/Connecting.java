282069c44ada981fb741903df120d01f280abd6b
MarcJus <jusseaumemarc@gmail.com>
2019-12-20 10:06:24 +0100

    le joueur ne recoit plus automatiquement un effet de potion lors de la connection

--- src/fr/marcjus/listeners/Connecting.java
+++ src/fr/marcjus/listeners/Connecting.java
@@ -12,10 +12,7 @@ public class Connecting implements Listener {
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		Player player = e.getPlayer();
-		//player.setInvulnerable(false);
-
-		player.removePotionEffect(PotionEffectType.SPEED);
-		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999, 1));
+		
 		e.setJoinMessage("§eBienvenue §2" + player.getName() + " §esur le serveur ! ");
 		player.sendMessage("§4Attention nous vous rappelons qu'il est interdit de tuer un villageois !");
 		
